1eb778fa94bcd795d7258b7963731602a142a514
Niyati Rajkotia <niyatirajkotia@gmail.com>
2026-03-23 03:05:16 +0530

    Maven project

---
+++ src/main/java/com/mycompany/mavendemo/Mavendemo.java
@@ -0,0 +1,12 @@
+package com.mycompany.mavendemo;
+
+/**
+ *
+ * @author Niyati Rajkotia
+ */
+public class Mavendemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
